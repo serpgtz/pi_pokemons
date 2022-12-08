e6e88c626230215017b8499a57d16d6021cee6d0
sergio <sheko3857@gmail.com>
2022-12-07 16:21:17 -0800

    se configuro variables de entorno para deploy

--- api/index.js
+++ api/index.js
@@ -20,11 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {typesApi} = require("./src/routes/utils")
+const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`server raised in port ${port}`); // eslint-disable-line no-console
     typesApi()
   });
 });
